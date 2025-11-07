724952c2792c34748655d14206ed0dbc521e3793
Gwill1337 <gwyllig@gmail.com>
2025-11-07 21:27:16 +0200

    Update test_batfish.py

--- tests/test_batfish.py
+++ tests/test_batfish.py
@@ -1,34 +1,57 @@
-#!/usr/bin/env python
-
-# Modules
+#!/usr/bin/env python3
 from pybatfish.client.commands import bf_init_snapshot, bf_session
-from pybatfish.question.question import load_questions
 from pybatfish.question import bfq
 import os
 
-# Variables
+# Настройки
 bf_address = "127.0.0.1"
-snapshot_path = "./snapshot"
+snapshot_path = "./snapshots/ci_net/s1"
 output_dir = "./output"
 
-# Body
-if __name__ == "__main__":
-    # Setting host to connect
-    bf_session.host = bf_address
+# Подключение к Batfish
+bf_session.host = bf_address
+bf_init_snapshot(snapshot_path, overwrite=True)
 
-    # Loading confgs and questions
-    bf_init_snapshot(snapshot_path, overwrite=True)
-    load_questions()
+# Пример запроса
+r = bfq.nodeProperties().answer().frame()
+print(r)
 
-    # Running questions
-    r = bfq.nodeProperties().answer().frame()
-    print(r)
+# Сохраняем результат
+os.makedirs(output_dir, exist_ok=True)
+r.to_csv(f"{output_dir}/results.csv")
 
-    # Saving output
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
 
-    r.to_csv(f"{output_dir}/results.csv")
+# #!/usr/bin/env python
+
+# # Modules
+# from pybatfish.client.commands import bf_init_snapshot, bf_session
+# from pybatfish.question.question import load_questions
+# from pybatfish.question import bfq
+# import os
+
+# # Variables
+# bf_address = "127.0.0.1"
+# snapshot_path = "./snapshot"
+# output_dir = "./output"
+
+# # Body
+# if __name__ == "__main__":
+#     # Setting host to connect
+#     bf_session.host = bf_address
+
+#     # Loading confgs and questions
+#     bf_init_snapshot(snapshot_path, overwrite=True)
+#     load_questions()
+
+#     # Running questions
+#     r = bfq.nodeProperties().answer().frame()
+#     print(r)
+
+#     # Saving output
+#     if not os.path.exists(output_dir):
+#         os.mkdir(output_dir)
+
+#     r.to_csv(f"{output_dir}/results.csv")
 
 # import time
 # from pybatfish.client.session import Session
